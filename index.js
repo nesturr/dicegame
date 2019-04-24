920a829cdeba04681ac4490271dcb44e525b7771
nesturr <49391363+nesturr@users.noreply.github.com>
2019-04-24 00:24:55 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ document.getElementsByClassName("button1")[0].onclick = dice;
 function dice(){
 var randomNumber1 = Math.floor(Math.random() * 6 + 1);
 var randomNumber2 = Math.floor(Math.random() * 6 + 1);
-var img = "images/dice" + randomNumber1 + ".png"
-var img2 = "images/dice" + randomNumber2 + ".png"
+var img = "dice" + randomNumber1 + ".png"
+var img2 = "dice" + randomNumber2 + ".png"
 document.getElementsByClassName("img1")[0].setAttribute("src", img);
 document.getElementsByClassName("img2")[0].setAttribute("src", img2);
 if(randomNumber1 > randomNumber2){
